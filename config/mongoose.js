d2d1e845417fc4bf78755354959ebdcd44e765b2
JoyCode4 <joywadhonkar4@gmail.com>
2023-10-07 17:25:37 +0530

    1.Install the package of the mongoose for ODM of mongoDB 2.MongoDB connection and adding it in the index.js file as db

---
+++ config/mongoose.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+// **It is My Logic to connect mongodb
+const db = main();
+
+async function main() {
+  try {
+    // put only 127.0.0.1:27017 don't put localhost
+    await mongoose.connect("mongodb://127.0.0.1:27017/socialApp");
+    console.log("MongoDB is Connected Successfully");
+  } catch (err) {
+    console.log("Error in Connecting MongoDB: " + err.message);
+  }
+}
+
+
+// **Sir's Logic to connect mongodb
+// mongoose.connect("mongodb://127.0.0.1:27017/socialApp")
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "Error connecting to MongoDB:"));
+
+// db.once("open", function() {
+//   console.log("Connected to Database :: MongoDB");
+// });
+
+
+
+module.exports = db;
