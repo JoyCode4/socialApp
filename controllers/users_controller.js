eeeb1f7e99e8edb731481552608c9eac33473ffc
JoyCode4 <joywadhonkar4@gmail.com>
2023-10-07 18:48:23 +0530

    1.created the view for the user signIn and SignUp and added the dummy controllers 2.installed the cookie-parser for further process

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,31 @@
+// render user profile page
 module.exports.profile = (req,res)=>{
     res.render("user_profile",{
         title:"User Profile"
     });
 }
+
+// render user sign In page or Login Page
+module.exports.signUp = (req,res)=>{
+    res.render("user_sign_up",{
+        title:"Social App | Sign Up"
+    })
+}
+
+// render user Sign Up page
+module.exports.signIn = (req,res)=>{
+    res.render("user_sign_in",{
+        title:"Social App | Sign In"
+    })
+}
+
+// get the sign up data
+module.exports.create = (req,res)=>{
+    console.log(req.body);
+}
+
+// sign in and create a session for the user
+module.exports.createSession = (req,res)=>{
+    console.log(req.body);
+}
+
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,7 @@ const users_controller = require("../controllers/users_controller");
 
 router.get("/profile",users_controller.profile);
 
+router.get("/sign-up",users_controller.signUp);
+router.get("/sign-in",users_controller.signIn);
+
 module.exports = router;
