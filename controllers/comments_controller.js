89176e3716a8758bc19f6f375c6032832bd614f9
JoyCode4 <joywadhonkar4@gmail.com>
2023-10-11 20:23:39 +0530

    1.created the create function to create the comment and comment is created in Comments Schema and push it to the Post Schema field of Comments array. 2.Use the populate method to call the comments with the user. 3.Render the Comments content and also the user of comment and displayed it

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,23 @@
 const Comment = require("../models/comment");
+const Post = require("../models/post");
 
-module.exports.create = async (req,res)=>{
-    console.log(req.body,req.user._id);
-    
-}
+module.exports.create = async (req, res) => {
+  console.log(req.body, req.user._id);
+  try {
+    const post = await Post.findById(req.body.post);
+    if (post) {
+      const comment = await Comment.create({
+        content: req.body.content,
+        post: req.body.post,
+        user: req.user._id,
+      });
+
+      post.comment.push(comment);
+      post.save();
+
+      res.redirect("/");
+    }
+  } catch (err) {
+    console.log("Error in creating Post : Error : " + err);
+  }
+};
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,18 @@
 const Post = require("../models/post");
-module.exports.home = async (req,res)=>{
-    // console.log(req.cookies); //getting cookie with name or key
-    // res.cookie("user_id",55); //setting cookie
+module.exports.home = async (req, res) => {
+  // console.log(req.cookies); //getting cookie with name or key
+  // res.cookie("user_id",55); //setting cookie
 
-
-    // populate the user of each post
-    const posts = await Post.find({}).populate("user").exec();
-
-    return res.render("home",{
-        title:"Social Media App",
-        posts:posts
+  // populate the user of each post
+  const posts = await Post.find({})
+    .populate("user")
+    .populate({
+      path: "comment",
+      populate: { path: "user" },
     })
-}
+    .exec();
+  return res.render("home", {
+    title: "Social Media App",
+    posts: posts,
+  });
+};
