e18ebe9e8b0fc3a0b41f76fafcae686f1a498103
JoyCode4 <joywadhonkar4@gmail.com>
2023-10-11 21:04:58 +0530

    1.Delete Controller for the Post and also comments of that function 2.Make a symbol to delete the Post 3.Crete destroy router with authentication for deletion of post make some changes in the ejs and css for display

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,17 +1,35 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
-module.exports.create=async (req,res)=>{
-    console.log(req.user);
-    console.log(req.body);
-    if(!req.user){
-        return res.redirect("/users/sign-in");
-    }
-    else{
-        const post = await Post.create({
-            content:req.body.content,
-            user:req.user._id
-        })
-        console.log(post);
+module.exports.create = async (req, res) => {
+  //   console.log(req.user);
+  //   console.log(req.body);
+  if (!req.user) {
+    return res.redirect("/users/sign-in");
+  } else {
+    const post = await Post.create({
+      content: req.body.content,
+      user: req.user._id,
+    });
+    // console.log(post);
+    return res.redirect("back");
+  }
+};
+
+module.exports.destroy = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      if (post.user == req.user.id) {
+        await Post.findByIdAndDelete(req.params.id);
+
+        await Comment.deleteMany({ post: req.params.id });
+
         return res.redirect("back");
+      }
+      return res.redirect("back");
     }
-}
+  } catch (err) {
+    console.log("Error while deleting Post: " + err);
+  }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 const posts_controller = require("../controllers/posts_controller");
 
-router.post("/create",passport.checkAuthentication,posts_controller.create);
+router.post("/create", passport.checkAuthentication, posts_controller.create);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  posts_controller.destroy
+);
 
 module.exports = router;
