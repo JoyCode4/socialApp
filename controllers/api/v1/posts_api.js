5b4b25ab84338ba4f01d44516cb16a4a9c304ed6
JoyCode4 <joywadhonkar4@gmail.com>
2023-10-15 16:59:39 +0530

    1.API creation is done for the post fecthing and the deletion of the post 2.Routes and Controller is created for the API-V1

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,41 @@
+const Post = require("../../../models/post");
+const Comment = require("../../../models/comment");
+
+module.exports.index = async (req, res) => {
+  const posts = await Post.find({})
+    .sort("-createdAt")
+    .populate("user")
+    .populate({
+      path: "comment",
+      populate: {
+        path: "user",
+      },
+    });
+  return res.status(200).json({
+    message: "List of Posts",
+    posts: posts,
+  });
+};
+
+module.exports.destroy = async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
+
+    // if (post.user == req.user.id) {
+    await Post.findByIdAndDelete(req.params.id);
+    await Comment.deleteMany({ post: req.params.id });
+    // req.flash("success", "Post Deleted Successfully!");
+    // return res.redirect("back");
+    // }
+    return res.status(200).json({
+      message: "Post Deleted Successfully",
+      post: post,
+    });
+  } catch (err) {
+    // console.log("Error while deleting Post: " + err);
+    // return res.redirect("back");
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const home_controller = require("../controllers/home_controller");
 
 console.log("Routes are loaded");
 
-router.get("/",home_controller.home);
-router.use("/users",require("./users"));
-router.use("/posts",require("./posts"));
-router.use("/comments",require("./comments"))
+router.get("/", home_controller.home);
+router.use("/users", require("./users"));
+router.use("/posts", require("./posts"));
+router.use("/comments", require("./comments"));
 
-
+router.use("/api", require("./api"));
 
 module.exports = router;
